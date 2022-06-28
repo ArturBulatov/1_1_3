18c578efaa6884909bcf0ac6f3e74fdfbdc95b80
ArturBulatov <bulatov996@mail.ru>
2022-06-28 20:04:25 +0500

    С изменениями

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,60 +3,78 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Util util = new Util();
+
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        try {
-            Util.createUsersTable();
-        } catch (ClassNotFoundException | SQLException e) {
+        try(Statement stmt = util.connect().createStatement()) {
+            stmt.executeUpdate("CREATE TABLE if not exists UsersTable(id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT, " +
+                    "name VARCHAR(20), lastName VARCHAR(20), age TINYINT)");
+            System.out.println("Таблица создана.");
+            util.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try {
-            Util.dropUsersTable();
-        } catch (ClassNotFoundException | SQLException e) {
+        try(Statement stmt = util.connect().createStatement()){
+            stmt.executeUpdate("DROP TABLE if exists UsersTable");
+            util.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            Util.saveUser(name, lastName, age);
-        } catch (ClassNotFoundException | SQLException e) {
+        try(PreparedStatement preparedStatement = util.connect().prepareStatement("INSERT INTO UsersTable (name, lastName, age) Values (?, ?, ?)")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем - " + name + " добавлен в базу данных.");
+            util.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        try {
-            Util.removeUserById(id);
-        } catch (ClassNotFoundException | SQLException e) {
+        try(PreparedStatement preparedStatement = util.connect().prepareStatement("DELETE FROM UsersTable WHERE Id = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("User с номером id = " + id + " удален из базы данных.");
+            util.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public List<User> getAllUsers() {
         List<User> listUsers = new ArrayList<>();
-        try {
-            Util.getAllUsers(listUsers);
-        } catch (ClassNotFoundException | SQLException e) {
+        try (PreparedStatement preparedStatement = util.connect().prepareStatement("SELECT * FROM UsersTable")) {
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                listUsers.add(new User(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getByte(4)));
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return listUsers;
     }
 
     public void cleanUsersTable() {
-        try {
-            Util.cleanUsersTable();
-        } catch (ClassNotFoundException | SQLException e) {
+        try (Statement stmt = util.connect().createStatement()) {
+            stmt.executeUpdate("DELETE FROM UsersTable");
+            System.out.println("Таблица удалена.");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,77 +6,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
+    private static final String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String LOGIN = "root1";
+    private static final String PASSWORD = "root1";
+    Connection connection = null;
+
     public Util() {
     }
-    public static void createUsersTable() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/mysql";
-        String login = "root1";
-        String password = "root1";
-        String sqlCommand = "CREATE TABLE if not exists UsersTable(id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(20), age TINYINT)";
-        try(Connection con = DriverManager.getConnection(url, login, password); Statement stmt = con.createStatement()) {
-            stmt.executeUpdate(sqlCommand);
-            System.out.println("Таблица создана.");
+
+    public Connection connect() {
+        try {
+            Class.forName(DATABASE_DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, LOGIN, PASSWORD);
+            //System.out.println("Соединение с базой данных установлено!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
+        return connection;
     }
-    public static void dropUsersTable() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/mysql";
-        String login = "root1";
-        String password = "root1";
-        String sqlCommand = "DROP TABLE if exists UsersTable";
-        try(Connection con = DriverManager.getConnection(url, login, password); Statement stmt = con.createStatement()){
-            stmt.executeUpdate(sqlCommand);
-        }
-    }
-    public static void saveUser(String n, String l, byte a) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/mysql";
-        String login = "root1";
-        String password = "root1";
-        try(Connection con = DriverManager.getConnection(url, login, password); Statement stmt = con.createStatement()) {
-            String sql = "INSERT INTO UsersTable (name, lastName, age) Values (?, ?, ?)";
-            PreparedStatement preparedStatement = con.prepareStatement(sql);
-            preparedStatement.setString(1, n);
-            preparedStatement.setString(2, l);
-            preparedStatement.setByte(3, a);
-            preparedStatement.executeUpdate();
-            System.out.println("User с именем - " + n + " добавлен в базу данных.");
-        }
-    }
-    public static List<User> getAllUsers(List<User> listUsers) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/mysql";
-        String login = "root1";
-        String password = "root1";
-        try (Connection con = DriverManager.getConnection(url, login, password); Statement stmt = con.createStatement()) {
-            ResultSet rs = stmt.executeQuery("SELECT * FROM UsersTable");
-            while (rs.next()) {
-                listUsers.add(new User(rs.getLong(1), rs.getString(2), rs.getString(3),rs.getByte(4)));
-            }
-        }
-        return listUsers;
-    }
-    public static void cleanUsersTable() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/mysql";
-        String login = "root1";
-        String password = "root1";
-        try(Connection con = DriverManager.getConnection(url, login, password); Statement stmt = con.createStatement()) {
-            stmt.executeUpdate("DELETE FROM UsersTable");
-            System.out.println("Таблица удалена.");
-        }
-    }
-    public static void removeUserById(long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/mysql";
-        String login = "root1";
-        String password = "root1";
-        try(Connection con = DriverManager.getConnection(url, login, password); Statement stmt = con.createStatement()) {
-            PreparedStatement preparedStatement = con.prepareStatement("DELETE FROM UsersTable WHERE Id = ?");
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-            System.out.println("User с номером id = " + id + " удален из базы данных.");
+
+    public void close() {
+        try {
+            connection.close();
+            //System.out.println("Соединение с базой данных разорвано!");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
